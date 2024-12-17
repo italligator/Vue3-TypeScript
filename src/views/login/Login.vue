e575c241215d098d303f1ef842b34ef971e0a469
ironarmy <zhangtiejun@pkutech.co.jp>
2024-12-17 16:48:36 +0900

    ログイン画面への修正

---
+++ src/views/login/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login">
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model="email" class="form-control" placeholder="Enter email" />
+      </div>
+    </form>
+  </div>
+</template>
+<script lang="ts"></script>
